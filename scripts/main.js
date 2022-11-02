1193896ba3651f9aa2d154580d5f284da9ec09be
Mateusz Jablonski <mjablonski.praca@gmail.com>
2022-11-02 21:15:14 +0100

    code 02.11.2022

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+const myImage = document.querySelector('img');
+
+myImage.onclick = () => {
+	const mySrc = myImage.getAttribute('src');
+	if (mySrc === 'images/white-to-dark.jpg') {
+		myImage.setAttribute('src', 'images/white-to-dark-changed.jpg');
+	} else {
+		myImage.setAttribute('src', 'images/white-to-dark.jpg');
+	}
+};
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+	const myName = prompt('Please enter your name.');
+	if (!myName) {
+		setUserName();
+	} else {
+		localStorage.setItem('name', myName);
+		myHeading.textContent = `ID welcomes ${myName}`;
+	}
+}
+
+
+if (!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	const storedName = localStorage.getItem('name');
+	myHeading.textContent = `ID welcomes ${storedName}`;
+}
+
+myButton.onclick = () => {
+	setUserName();
+};
